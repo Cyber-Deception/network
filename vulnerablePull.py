558064d2dccd1ad8b7bb0d781f9a615cbbdd5c73
ankur8931 <achaud16@asu.edu>
2019-10-22 08:54:42 -0700

    example network by Jake

---
+++ vulnerablePull.py
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo docker pull jvhoof/badstore-docker
+sudo docker pull szsecurity/mutillidae
+sudo docker pull ubuntu
+sudo docker pull fedora
+sudo docker pull kalilinux-kali-linux-docker
+sudo docker pull vulnerables/web-dwva
+sudo docker pull peakkk/metasploitable
+sudo docker pull infoslack/dvwa
